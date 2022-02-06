e98eb4635d8a241dd0a396d2451b1dc89c5292ca
PCov3r <38764918+PCov3r@users.noreply.github.com>
2022-02-06 19:50:47 +0100

    Added svg module

---
+++ getsvg.py
@@ -0,0 +1,19 @@
+from cv2 import *
+from svgpathtools import svg2paths2,wsvg
+
+# Extract path from svg
+def fromsvg(path):
+    paths, attributes, svg_attributes = svg2paths2(path)
+    # take the first path
+    path = paths[0]
+
+    n = 700  # number of samples
+
+    pts = []
+    # add points affixes 
+    for i in range(0,n+1):
+        f = i/n
+        complex_point = path.point(f)
+        pts.append(complex(complex_point.real,complex_point.imag))
+
+    return(pts)
